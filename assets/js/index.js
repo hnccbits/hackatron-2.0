978cd56693bea1d516fb3e64f913b8527d1de48f
Zeeshan-2k1 <ashraf2k1zeeshan@gmail.com>
2021-02-13 00:41:22 +0530

    Header Animations

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,30 @@
 $(document).ready(function () {
   $('.detail-register').tilt({
-    axis: x,
+    axis: 'x',
   });
+});
+
+const translate = document.querySelectorAll('.translate');
+const header = document.querySelector('.header');
+const logo = document.querySelector('.logo');
+const layerBottom = document.querySelector('.layer');
+const details = document.querySelector('.details');
+
+const detailHeight = details.offsetHeight;
+const headerHeight = header.offsetHeight;
+
+window.addEventListener('scroll', () => {
+  let scroll = window.pageYOffset;
+  let mainY = details.getBoundingClientRect();
+
+  translate.forEach((element) => {
+    let speed = element.dataset.speed;
+    element.style.transform = `translateY(${scroll * speed}px)`;
+  });
+
+  logo.style.opacity = -(scroll / headerHeight) + 1;
+
+  if (scroll * 0.12 >= 50) {
+    layerBottom.style.height = `${scroll * 0.12}px`;
+  } else layerBottom.style.height = `50px`;
 });
